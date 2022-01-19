915ab4426cb8c109f12dbb650ea88923fba3dcda
JunZheng007 <jzheng0721@gmail.com>
2022-01-19 14:57:24 -0500

    change the style of authHeader

--- client/src/components/AuthHeader/AuthHeader.tsx
+++ client/src/components/AuthHeader/AuthHeader.tsx
@@ -1,4 +1,3 @@
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
@@ -14,18 +13,13 @@ const AuthFooter = ({ linkTo, asideText, btnText }: Props): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <Box
-      p={1}
-      display="flex"
-      justifyContent="flex-end"
-      alignSelf="flex-end"
-      marginRight={5}
-      className={classes.authHeader}
-    >
-      <Typography className={classes.accAside}>{asideText}</Typography>
-      <Button component={Link} to={linkTo} color="inherit" className={classes.accBtn} variant="contained">
-        {btnText}
-      </Button>
+    <Box p={1} display="flex" justifyContent="center" alignSelf="center" className={classes.authHeader}>
+      <Typography className={classes.accAside}>
+        {asideText + '\xa0'}
+        <Link to={linkTo} color="inherit" className={classes.accBtn}>
+          {btnText}
+        </Link>
+      </Typography>
     </Box>
   );
 };
