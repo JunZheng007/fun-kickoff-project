d8fe1321224bafd1b9998c6602bf6b8f32b16f6d
JunZheng007 <jzheng0721@gmail.com>
2022-01-20 15:16:15 -0500

    design profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: Number,
+  },
+  address: {
+    type: String,
+  },
+  photo: {
+    type: String,
+  },
+  availability: {
+    type: [String],
+    default: [],
+  },
+  booking_date: {
+    type: [Date],
+    default: [],
+  },
+  rank: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
